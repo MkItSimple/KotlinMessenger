4f83b3657fdb31dd937fda000dbde03f7382d0ee
MkItSimple <choanania1@gmail.com>
2020-03-22 20:56:12 +0800

    Branch03

--- app/src/main/java/com/example/kotlinmessenger/messages/NewMessageActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/messages/NewMessageActivity.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinmessenger.messages
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinmessenger.R
 import com.example.kotlinmessenger.models.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -24,14 +26,6 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Select User"
 
-        val adapter = GroupAdapter<ViewHolder>()
-
-//        adapter.add(UserItem())
-//        adapter.add(UserItem())
-//        adapter.add(UserItem())
-
-        recyclerview_newmessage.adapter = adapter
-
         fetchUsers()
     }
 
@@ -42,30 +36,45 @@ class NewMessageActivity : AppCompatActivity() {
     private fun fetchUsers() {
         val ref = FirebaseDatabase.getInstance().getReference("/users")
         ref.addListenerForSingleValueEvent(object: ValueEventListener {
+
             override fun onDataChange(p0: DataSnapshot) {
                 val adapter = GroupAdapter<ViewHolder>()
+                val uid = FirebaseAuth.getInstance().uid
 
-                p0.children.forEach{
+                p0.children.forEach {
                     Log.d("NewMessage", it.toString())
                     val user = it.getValue(User::class.java)
-                    if (user != null) {
+                    if (user != null && user.uid != uid) {
                         adapter.add(UserItem(user))
                     }
+                }
+
+                adapter.setOnItemClickListener { item, view ->
+
+                    val userItem = item as UserItem
+
+                    val intent = Intent(view.context, ChatLogActivity::class.java)
+                    intent.putExtra(USER_KEY, userItem.user)
+                    startActivity(intent)
+
+                    finish()
+
                 }
 
                 recyclerview_newmessage.adapter = adapter
             }
 
             override fun onCancelled(p0: DatabaseError) {
+
             }
         })
     }
-
-
 }
-class UserItem(val user: User): Item<ViewHolder>(){
+
+class UserItem(val user: User): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.username_textview_new_message.text = user.username
+
         Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
     }
 
--- app/src/main/java/com/example/kotlinmessenger/registerlogin/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/registerlogin/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinmessenger.registerlogin
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinmessenger.R
+import com.example.kotlinmessenger.messages.LatestMessagesActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -15,16 +18,37 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_button_login.setOnClickListener {
-            val email = email_edittext_login.text.toString()
-            val password = password_edittext_login.text.toString()
-
-            Log.d("Login", "Attempt login with email/pw: $email/***")
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            performLogin()
         }
 
         back_to_register_textview.setOnClickListener{
             finish()
         }
+    }
+
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+//                Log.d("Login", "Successfully logged in: ${it.result.user.uid}")
+                Log.d("Login", "Successfully logged in: ")
+
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
